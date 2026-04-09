6ef5ba25134767aaa4a6f2d9d4c09eb63e1971ba
Nahuel Palacios <nahuel.palacios@piconsulting.com.ar>
2026-04-08 23:21:47 -0300

    change

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './styles/globals.css'
-import App from './App/App.jsx'
+import App from './app/App.jsx'
 import { NotificationProvider } from './notifications/NotificationProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
